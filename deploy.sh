0b649b0c008d9c31aa1f75b29df2fc038195bdac
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-10-10 15:29:56 +0200

    misc

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# PROJECT_NAME="Name or your frontend project, for example movie --> folder you created under /var/www"
-# DROPLET_URL="URL for your droplet"
-echo -n "please enter the project name as it appears on the server in /var/www/"
-read -r 
-PROJECT_NAME=$REPLY
-echo -n "please enter the droplet url (e.g myserver.dk)"
-read -r
-DROPLET_URL=$REPLY
-
 echo "##############################"
 echo "Building the frontend project"
 echo "##############################"
@@ -18,5 +9,4 @@ echo "##############################"
 echo "Deploying Frontend project..."
 echo "##############################"
 
-scp -r ./build/* root@$DROPLET_URL:/var/www/$PROJECT_NAME
-
+scp -r ./build/* root@164.90.231.187:/var/www/person
